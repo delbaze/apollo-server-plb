16f32c56aeac720ff25821499c9717ea2108df26
delbaze <56109313+delbaze@users.noreply.github.com>
2024-11-02 19:44:09 +0100

    apres author

---
+++ backend/src/resolvers/author.resolver.ts
@@ -0,0 +1,32 @@
+export const authors = [
+  {
+    id: 1,
+    name: "Gabriel Garcia Marquez",
+    age: 87,
+    nationality: "Colombian",
+  },
+  {
+    id: 2,
+    name: "Haruki Murakami",
+    age: 74,
+    nationality: "Japanese",
+  },
+  {
+    id: 3,
+    name: "Toni Morrison",
+    age: 88,
+    nationality: "American",
+  },
+  {
+    id: 4,
+    name: "George Orwell",
+    age: 46,
+    nationality: "British",
+  },
+];
+
+export default {
+  Query: {
+    authors: () => authors,
+  },
+};
--- backend/src/resolvers/book.resolver.ts
+++ backend/src/resolvers/book.resolver.ts
@@ -1,18 +1,20 @@
+import { authors } from "./author.resolver";
+
 const books = [
-    {
-      title: "Titre 1",
-      author: "Auteur 1",
-    },
-    {
-      title: "Titre 2",
-      author: "Auteur 2",
-    },
-  ];
+  {
+    id: "1",
+    title: "Titre 1",
+    author: authors[0],
+  },
+  {
+    id: "2",
+    title: "Titre 2",
+    author: authors[1],
+  },
+];
 
-  
 export default {
-    Query: {
-        books: () => books
-    }
-
-}
+  Query: {
+    books: () => books,
+  },
+};
